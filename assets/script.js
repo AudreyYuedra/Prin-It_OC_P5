b9965e77d26ff4b048bfb381aad81c011bf1467f
Audrey Luquet <audrey.luquet@tutanota.com>
2023-09-15 15:17:29 +0200

    tentative affichage tableau slide

--- assets/script.js
+++ assets/script.js
@@ -16,69 +16,94 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+];
+
 
 /*** Récupération id des élèments du fichier html ***/
-const arrowLeft = document.getElementById("arrowLeft")
-const arrowRight = document.getElementById("arrowRight")
-const dotSlide = document.getElementById("dotSlide")	// conteneur bullets points
-const images = document.getElementsByClassName("banner-img")
+const arrowLeft = document.getElementById("arrowLeft");
+const arrowRight = document.getElementById("arrowRight");
+const dotSlide = document.getElementById("dotSlide");	// conteneur bullets points
+let images = document.getElementsByClassName("banner-img");
+let tag = document.getElementsByClassName("banner p");
 
 /*** Variables ***/
-let currentImg = 0
-let currentTagLine = 0
+let currentImg = 0;
+let currentTagLine = 0;
 
 /*** Ajouter les bullet points au carrousel ***/
-function addDots (index) {		// !!!! les bullets ne sont pas visibles sur le site (étape 3 non terminée) !!!!
-	console.log("add new dot", index)
+function addDots (index) {
+	console.log("add new dot", index);
 
-	let newDot = document.createElement("div")		// créer un nouvel élèment "div" pour dot
-	dotSlide.appendChild(newDot)              		// placer "div" dans élèment "dotSlide"
-	newDot.classList.add("dot")						// ajouter sélecteur css ".dot" à "div"
+	let newDot = document.createElement("div");			// créer un nouvel élèment "div" pour dot
+	dotSlide.appendChild(newDot);						// placer "div" dans élèment "dotSlide"
+	newDot.classList.add("dot");						// ajouter sélecteur css ".dot" à "div"
 
-	if (index === currentImg) {                   	// SI slide affichée sur le site
-		newDot.classList.add("dot_selected")  		// ajouter class ".dot_selected" à "div"
-	} else {								   		// SINON 
-		newDot.classList.remove("dot_selected")		// enlever class ".dot_selected" à "div"
+	if (index === currentImg) {							// SI slide affichée sur le site
+		newDot.classList.add("dot_selected");			// ajouter class ".dot_selected" à "div"
+	} else {											// SINON 
+		newDot.classList.remove("dot_selected");			// enlever class ".dot_selected" à "div"
 	}
 }
 
-for (let i = 0; i < slides.length; i++) {			// répétition création "div" selon nb slides
-	addDots(i)
+for (let i = 0; i < slides.length; i++) {				// répétition création "div" selon nb slides
+	addDots(i);
 }
 
 /*** Comportement du carrousel ***/
-function nextImage() {
-	currentImg = currentImg + 1
-	if (currentImg >= slides.length) {
-		currentImg = 0
-	}
-	currentTagLine = currentTagLine + 1 
-	if (currentTagLine >= slides.length) {
-		currentTagLine = 0
-	}
+function showSlider () {
+	images = slide[i];
+	tag.innerHTML = tagSlide[i];
 }
 
-function previousImg() {
-	slides[image] = currentImg - 1
+function nextImage() {									// avancer d'une image par click
+	currentImg++;
+	if (currentImg >= slides.length) {
+		currentImg = 0;
+	}
+	
+	currentTagLine++;
+	if (currentTagLine >= slides.length) {
+		currentTagLine = 0;
+	}
+
+	showSlider();
+}
+
+function previousImg() {								// reculer d'une image par click
+	currentImg--;
 	if (currentImg <= slides.length) {
-		currentImg = 3
+		currentImg = 3;
 	}
-	slides[tagLine] = currentTagLine - 1
+
+	currentTagLine--;
 	if (currentTagLine <= slides.length) {
-		currentTagLine = 3
+		currentTagLine = 3;
 	}
+
+	showSlider();
 }
 
 /*** Navigation du carrousel ***/					// évènement au click
 // Flèche de droite
 arrowRight.addEventListener("click", function() {
     nextImage();
-	console.log("Vous avez cliqué sur la flèche droite.")
+	console.log("Vous avez cliqué sur la flèche droite.");
 })
 // Flèche de gauche
 arrowLeft.addEventListener("click", function() {
     previousImg();
-	console.log("Vous avez cliqué sur la flèche gauche.")
+	console.log("Vous avez cliqué sur la flèche gauche.");
 })
 
+
+
+
+/*
+const imageSlide = ["slide1.jpg", "slide2.jpg", "slide3.jpg", "slide4.png"]
+const tagSlide = [
+	"Impressions tous formats <span>en boutique et en ligne</span>",
+	"Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+	"Grand choix de couleurs <span>de CMJN aux pantones</span>",
+	"Autocollants <span>avec découpe laser sur mesure</span>"
+]
+*/
