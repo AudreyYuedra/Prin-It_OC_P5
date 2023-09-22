c6a766f7ba671dc9ef8d9c495ac8aba6a1e6c563
Audrey Luquet <audrey.luquet@tutanota.com>
2023-09-22 16:42:10 +0200

    réorganistion fonctions + main js

--- assets/script.js
+++ assets/script.js
@@ -18,152 +18,147 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-];
+]
 // Récupération id des élèments du fichier html
-const arrowLeft = document.getElementById("arrowLeft");
-const arrowRight = document.getElementById("arrowRight");
-const dotSlide = document.getElementById("dotSlide");	// conteneur bullets points
-const banner = document.getElementsByClassName("banner-img");
-const tagLine = document.getElementsByClassName("banner > p");
+const arrowLeft = document.getElementById("arrowLeft")
+const arrowRight = document.getElementById("arrowRight")
+const dotSlide = document.getElementById("dotSlide")	// conteneur bullets points
+const bannerImg = document.getElementsByClassName("banner-img")
+const tagLine = document.getElementsByClassName("banner > p")
 
 
 //********** VARIABLES **********
 
-let counter = 0;
-let arrayLength = slides.length;
-
-//let currentImg = slides[0].image;
-//let currentTagLine = slides[0].tagLine;
+let counter = 0
 
 
 // ********** FUNCTIONS **********
 
-// Ajouter les bullet points au carrousel
-function addDots(index) {
-	console.log("add new dot", index);
+//*** Ajouter les bullet points au carrousel
+function addDots(i) {
+	console.log("add new dot", i)
 
 	// créer un nouvel élèment "div" pour dot
-	let newDot = document.createElement("div");
+	let dot = document.createElement("div")
 	// placer "div" dans élèment "dotSlide"
-	dotSlide.appendChild(newDot);
+	dotSlide.appendChild(dot)
 	// ajouter sélecteur css ".dot" à "div"
-	newDot.classList.add("dot");
+	dot.classList.add("dot")
 
-	// SI slide affichée sur le site
-	if (index === 0) {
+	// SI slide d'origine affichée sur le site
+	if (i === 0) {
 		// ajouter class ".dot_selected" à "div"
-		newDot.classList.add("dot_selected");
-	} 
-	
-	
-	/*else {											// SINON 
+		dot.classList.add("dot_selected")
+
+		console.log("dot " + counter + " affiché")
+	} 	/*else {											// SINON 
 		newDot.classList.remove("dot_selected");		// enlever class ".dot_selected" à "div"
 	}*/
 }
-// Déplacer le slide à gauche
-function goPrevious() {
-	counter --;
-	//enlever la class "dot_selected" du dot d'origine
-	dot[counter].classList.remove("dot_selected");
 
+//*** Déplacer le slide à gauche
+function dotPrevious (index) {
+	//lien récupération dot
+	dot[counter] = slides[counter]
+
+	index --
+	//nouveau dot ajout .dot_selected
+	dot[counter --].classList.add("dot_selected")
+	//.dot_selected annulé
+	dot[counter].classList.remove("dot_selected")
+
+	console.log("dot " + counter + " affiché")
+}
+
+function imgPrevious() {
+	bannerImg.src = "./assets/images/slideshow/" + slides[counter].image
+
+	slides.image --
+	console.log("image " + slides[counter].image + " affiché")
+}
+
+function tagLinePrevious () {
+	tagLine.innerHTML = slides[counter].tagLine
+
+	slides.tagLine --
+	console.log("tagLine " + slides[counter].tagLine + " affiché")
+}
+
+function showPrevious() {
+	//enlever la class "dot_selected" du dot plus affcihé
+	dot[counter].classList.remove("dot_selected")
+
+	counter --
+	//défilement infini
 	if (counter === -1) {
-		counter = arrayLength -1;
+		counter = arrayLength[3]
 	}
 
 	//lien dossier images avec numérotation du slide
-	banner.src = "./assets/images/slideshow/" + slides[counter].image;
+	bannerImg.src = "./assets/images/slideshow/" + slides[counter].image;
 	//lien tableau tagLine avec numérotation du slide
 	tagLine.innerHTML = slides[counter].tagLine;
 	//ajouter class "dot-selected" au nouveau dot affiché
 	dot[counter].classList.add("dot-selected");
 }
-// Déplacer le slide à droite
-function goPrevious() {
-	counter ++;
-	//enlever la class "dot_selected" du dot d'origine
-	dot[counter].classList.remove("dot_selected");
 
-	if (counter === arrayLength) {
-		counter = 0;
-	}
+//*** Déplacer le slide à droite
+function dotNext (index) {
+	//lien récupération dot
+	dot[counter] = slides[counter]
 
-	//lien dossier images avec numérotation du slide
-	banner.src = "./assets/images/slideshow/" + slides[counter].image;
-	//lien tableau tagLine avec numérotation du slide
-	tagLine.innerHTML = slides[counter].tagLine;
-	//ajouter class "dot-selected" au nouveau dot affiché
-	dot[counter].classList.add("dot-selected");
-}
-//Activer le mouvement du slide au click sur les flèches
-function moveSlide() {
-	//liens paramètres slide d'origine
-	banner.src = "./assets/images/slideshow/" + slides[0].image;
-	tagLine.innerHTML = slides[0].tagLine;
-	dot[0].classList.add("dot-selected");
+	index ++
+	//nouveau dot ajout .dot_selected
+	dot[counter ++].classList.add("dot_selected")
+	//.dot_selected annulé
+	dot[counter].classList.remove("dot_selected")
 
-	arrowLeft.addEventListener("click", goPrevious);
-	arrowRight.addEventListener("click", goNext);
-	console.log("Vous avez cliqué sur la flèche.")
+	console.log("dot " + counter + " affiché")
 }
 
-/*function changeDots (index) {
-	let index = addDots(index);						// récupérer l'index - 
-	console.log("dot recuperation");			// console.log
-	index++;									// ajouter index +1
-	console.log("index dot +1");									// console.log
-	getElementsByClassName("dot_selected");	// getclassname + 
-	if (index++) {
+function imgNext() {
+	bannerImg.src = "./assets/images/slideshow/" + slides[counter].image
 
-	}					//appliquer la classe dot_selected
-	//for () {}					// appliquer check entre 0 et 4
-
-}*/
-
-/*** Comportement du carrousel ***/
-/*
-function next() {	
-								// avancer d'une image par click
-	currentImg++;
-	console.log()
-	if (currentImg >= slides.length) {
-		currentImg = 0;
-	}
-	
-	currentTagLine++;
-	if (currentTagLine >= slides.length) {
-		currentTagLine = 0;
-	}
-
-	showSlider();
+	slides.image ++
+	console.log("image " + slides[counter].image + " affiché")
 }
 
-function previousImg() {								// reculer d'une image par click
-	currentImg--;
-	if (currentImg <= imageSlide.length) {
-		currentImg = 3;
-	}
+function tagLineNext () {
+	tagLine.innerHTML = slides[counter].tagLine
 
-	currentTagLine--;
-	if (currentTagLine <= tagSlide.length) {
-		currentTagLine = 3;
-	}
-
-	showSlider();
+	slides.tagLine ++
+	console.log("tagLine " + slides[counter].tagLine + " affiché")
 }
-*/
-/*** Navigation du carrousel ***/						// évènement au click
-// Flèche de droite
 
-// Flèche de gauche
-/*arrowLeft.addEventListener("click", function() {
-    previousImg(currentImg, currentTagLine);
-	console.log("Vous avez cliqué sur la flèche gauche.");
-})*/
+function showNext() {
+	counter ++
+	dotNext()
+	imgNext()
+
+	//défilement infini
+	if (counter >= slides.length) {
+		counter = 0
+	}
+}
 
 
-// ********** OTHERS **********
+// ********** MAIN **********
 
-// Répétition création "div" selon le nombre de slides
+//*** Répétition création "div" selon le nombre de slides
 for (let i = 0; i < slides.length; i++) {			
-	addDots(i);
+	addDots(i)
 }
+
+//***
+const dot = document.querySelectorAll(".dot")
+
+
+//*** Activer le mouvement du slide au click sur les flèches
+arrowLeft.addEventListener("click", function() {
+	console.log("Vous avez cliqué sur la flèche gauche !")
+	showPrevious()
+});
+arrowRight.addEventListener("click", function() {
+	console.log("Vous avez cliqué sur la flèche droite !")
+	showNext()
+});
